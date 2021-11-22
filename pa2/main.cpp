e6da51ff4e9dd9ce156a17243c19815fc2457019
oceanofglitta <oceanofglitta@gmail.com>
2021-11-22 17:23:39 +0900

    First Commit

---
+++ pa2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pa2
+//
+//  Created by 박용욱 on 2021/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
