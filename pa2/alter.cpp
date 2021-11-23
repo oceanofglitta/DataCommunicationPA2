4644018649c50f77b11bab98a4fb29ed449cb50a
oceanofglitta <oceanofglitta@gmail.com>
2021-11-23 22:46:28 +0900

    Second commit"
    ;
    ;

---
+++ pa2/alter.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string message;
+    string polynomial;
+
+    cin >> message >> polynomial;
+
+    message[0] = (message[0]-'0') ? '0' : '1';
+
+    cout << message << endl << polynomial;
+
+    return 0;
+}
